5cea63b7eec4c10b39f7ce81148bfbfc452ae58b
kyngmn <79073979+kyngmn@users.noreply.github.com>
2021-04-07 02:11:50 +0900

    Trailer 관련 파일 업데이트!

--- netflix/src/components/index.js
+++ netflix/src/components/index.js
@@ -1,9 +1,6 @@
 import Header from "./Header";
 import Trailer from "./Trailer";
-import TrailerTitle from "./TrailerTitle";
 import LocoRow from "./LocoRow";
 import Footer from "./Footer";
-import PosterRow from "./PosterRow";
-// import Banner from "./Banner";
 
-export { Header, Trailer, TrailerTitle, LocoRow, Footer, PosterRow };
+export { Header, Trailer, LocoRow, Footer };
